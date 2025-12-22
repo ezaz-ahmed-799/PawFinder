baf4c21b4e7f2ef44c45b80c2c04bfe090a7c367
PortfolioDev <ezazpro777@gmail.com>
2025-12-22 16:01:19 +0530

    Add PawFinder as normal project folder

---
+++ PawFinder/src/main/java/com/neo/pawFinder/PawFinderApplication.java
@@ -0,0 +1,13 @@
+package com.neo.pawFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawFinderApplication.class, args);
+	}
+
+}
